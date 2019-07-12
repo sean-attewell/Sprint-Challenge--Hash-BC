ed023b91d7c036694b468abe6313b39abf50d7ac
sean-attewell <sean.attewell@gmail.com>
2019-07-12 10:45:29 +0100

    hash tables ex 1 tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,14 +7,49 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
 
     """
     YOUR CODE HERE
     """
 
+    index = 0
+
+    for item in weights:
+        target = limit - item
+        key_already_in_table = hash_table_retrieve(ht, item)
+        # need first or condition because 0 isn't considered true
+        if (key_already_in_table == 0 or key_already_in_table) and (limit - item == target):
+            return(index, key_already_in_table)
+        hash_table_insert(ht, item, index)
+        index += 1    
+
+    # index = 0
+
+    for item in weights:
+        target = limit - item
+        print('target', target)
+        # hash_table_remove(ht, item)
+        target_index = hash_table_retrieve(ht, target)
+        # hash_table_insert(ht, item, index)
+        # index += 1 
+        print('target_index', target_index)
+
+        if target_index != None:
+            item_index = hash_table_retrieve(ht, item)
+            print('item_index', item_index)
+
+            if item_index > target_index:
+                print('(item_index, target_index)', (item_index, target_index))
+                return (item_index, target_index)
+            else:
+                print('(target_index, item_index)', (target_index, item_index))
+                return (target_index, item_index)
+
+
     return None
 
+# get_indices_of_item_weights([4, 4], 2, 8)
 
 def print_answer(answer):
     if answer is None:
